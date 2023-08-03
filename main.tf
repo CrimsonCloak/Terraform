baf9317e14267b0eb2bc3e9fa477aa33fdc64ac0
CrimsonCloak <alexander.veldeman.999@gmail.com>
2023-08-03 11:22:19 +0200

    Added use of some variables for container (ports and name)

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
   name  = var.container_name
 
-  ports {
+ /* ports {
     internal = 80
     external = 8000
-  }
+  } */
 }
