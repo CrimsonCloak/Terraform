148b118a3a1afba7c68bb9afbb8fca472b907737
CrimsonCloak <alexander.veldeman.999@gmail.com>
2023-08-18 21:16:31 +0200

    Messing with docker network

--- main.tf
+++ main.tf
@@ -14,7 +14,10 @@ provider "docker" {}
 variable pwd {
   default = "/home/tux/Desktop/Terraform_experimenting"
 }
-
+ # Make network
+resource "docker_network" "private_network" {
+  name = "stardew"
+}
 
 resource "docker_image" "prometheus" {
   name         = "prom/prometheus"
@@ -24,6 +27,13 @@ resource "docker_image" "prometheus" {
 resource "docker_container" "prometheus" {
   image = docker_image.prometheus.image_id
   name  = "prometheus-test"
+  networks_advanced {
+    name = "stardew"
+  }
+  host {
+    host = "prometheus"
+    ip = "10.0.0.2"
+  }
   ports {
     internal = 9090
     external = 9090
@@ -46,6 +56,13 @@ keep_locally = false
 resource "docker_container" "grafana" {
   image = docker_image.grafana.image_id
   name = "grafana-test"
+  networks_advanced {
+    name = "stardew"
+  }
+  host {
+    host = "grafana"
+    ip = "10.0.0.4"
+  }
   ports {
     internal = 3000
     external = 3000
@@ -66,6 +83,13 @@ keep_locally = false
 resource "docker_container" "stardew" {
 image = docker_image.stardew.image_id
 name = "stardew-test"
+networks_advanced {
+    name = "stardew"
+  }
+host {
+    host = "stardew"
+    ip = "10.0.0.3"
+  }
 ports {
     internal = 9321
     external = 9321
